4f623f0bdc981be2a95ce1edde62229defa740b7
Dogan Karatas <dogan.karatas@airties.com>
2019-05-17 14:50:26 +0300

    Initial dummy commit

---
+++ phonebook/target/debug/deps/phonebook-72886596a8365687.d
@@ -0,0 +1,5 @@
+/home/dogan/Documents/rust-phonebook-demo/phonebook/target/debug/deps/phonebook-72886596a8365687: src/main.rs
+
+/home/dogan/Documents/rust-phonebook-demo/phonebook/target/debug/deps/phonebook-72886596a8365687.d: src/main.rs
+
+src/main.rs:
---
+++ phonebook/target/debug/phonebook.d
@@ -0,0 +1,1 @@
+/home/dogan/Documents/rust-phonebook-demo/phonebook/target/debug/phonebook: /home/dogan/Documents/rust-phonebook-demo/phonebook/src/main.rs
